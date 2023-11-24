446d4a2b5c1bd569b872c04f2db73a19c509b83e
KillzXGaming <millernathan35@gmail.com>
2023-11-23 21:06:11 -0500

    Fix culture info decimal places

--- PartsInjector/Program.cs
+++ PartsInjector/Program.cs
@@ -9,6 +9,8 @@ using Syroot.NintenTools.MarioKart8.BinData.Parts;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
+using System.Threading;
+using System.Globalization;
 
 namespace PartsInjector
 {
@@ -36,6 +38,16 @@ namespace PartsInjector
 
         static void Main(string[] args)
         {
+            int bodyID = 0;
+            int tireID = 1;
+
+            Runtime.BinFile = new BinFile("Parts.bin");
+
+            Runtime.BodyIndex = 0;
+            Runtime.TireIndex = 1;
+
+            var t = Runtime.GetBodyParts()[bodyID].GetBodyTireObjects()[tireID];
+
             if (args.Length == 0 || args.Contains("-h") || args.Contains("-help"))
             {
                 Console.WriteLine("Tool made by KillzXGaming with the help of Syroot for bin library.");
@@ -89,6 +101,8 @@ namespace PartsInjector
 
         static void Extract(string fileName)
         {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+
             Runtime.BinFile = new BinFile(fileName);
 
             if (!Directory.Exists("Body"))
@@ -104,6 +118,11 @@ namespace PartsInjector
             converters.Add(new Vector3Converter());
             converters.Add(new Vector4Converter());
             converters.Add(new Vector4BConverter());
+
+            var settings = new JsonSerializerSettings()
+            {
+                Converters = converters.ToArray(),
+            };
 
             var bodyParts = Runtime.GetBodyParts();
             var tireParts = Runtime.GetTireParts();
@@ -124,22 +143,22 @@ namespace PartsInjector
                 if (!Directory.Exists($"Body/{bod.PartName}/GliderParams"))
                     Directory.CreateDirectory($"Body/{bod.PartName}/GliderParams");
 
-                var json = JsonConvert.SerializeObject(bod, Formatting.Indented, converters.ToArray());
+                var json = JsonConvert.SerializeObject(bod, Formatting.Indented, settings);
                 File.WriteAllText($"Body/{bod.PartName}/{bod.PartName}.json", json);
 
                 foreach (var tire in bod.GetBodyTireObjects())
                 {
-                    json = JsonConvert.SerializeObject(tire, Formatting.Indented, converters.ToArray());
+                    json = JsonConvert.SerializeObject(tire, Formatting.Indented, settings);
                     File.WriteAllText($"Body/{bod.PartName}/TireParams/{tire.Name}.json", json);
                 }
                 foreach (var glider in bod.GetBodyGliderObjects())
                 {
-                    json = JsonConvert.SerializeObject(glider, Formatting.Indented, converters.ToArray());
+                    json = JsonConvert.SerializeObject(glider, Formatting.Indented, settings);
                     File.WriteAllText($"Body/{bod.PartName}/GliderParams/{glider.Name}.json", json);
                 }
                 foreach (var driver in bod.GetBodyDriverObjects())
                 {
-                    json = JsonConvert.SerializeObject(driver, Formatting.Indented, converters.ToArray());
+                    json = JsonConvert.SerializeObject(driver, Formatting.Indented, settings);
                     File.WriteAllText($"Body/{bod.PartName}/DriverParams/{driver.Name}.json", json);
                 }
             }
@@ -152,8 +171,18 @@ namespace PartsInjector
                 string folder = Path.Combine("Tire", tire.PartName);
                 if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
 
-                string json = JsonConvert.SerializeObject(tire, Formatting.Indented, converters.ToArray());
+                string json = JsonConvert.SerializeObject(tire, Formatting.Indented, settings);
                 File.WriteAllText(Path.Combine(folder, $"{tire.PartName}.json"), json);
+
+                string bodyFolder = Path.Combine(folder, "Body");
+                if (!Directory.Exists(bodyFolder))
+                    Directory.CreateDirectory(bodyFolder);
+
+                foreach (var tireBody in tire.GetTireBodyObjects())
+                {
+                    json = JsonConvert.SerializeObject(tireBody, Formatting.Indented, settings);
+                    File.WriteAllText(Path.Combine(bodyFolder, $"{tireBody.Name}.json"), json);
+                }
             }
 
             int gliderIndex = 0;
@@ -164,7 +193,7 @@ namespace PartsInjector
                 string folder = Path.Combine("Glider", glider.PartName);
                 if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
 
-                string json = JsonConvert.SerializeObject(glider, Formatting.Indented, converters.ToArray());
+                string json = JsonConvert.SerializeObject(glider, Formatting.Indented, settings);
                 File.WriteAllText(Path.Combine(folder, $"{glider.PartName}.json"), json);
             }
 
@@ -176,13 +205,15 @@ namespace PartsInjector
                 string folder = Path.Combine("Driver", driver.PartName);
                 if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
 
-                string json = JsonConvert.SerializeObject(driver, Formatting.Indented, converters.ToArray());
+                string json = JsonConvert.SerializeObject(driver, Formatting.Indented, settings);
                 File.WriteAllText(Path.Combine(folder, $"{driver.PartName}.json"), json);
             }
         }
 
         static void Inject(string fileName, string outputFilePath)
         {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+
             Runtime.BinFile = new BinFile(fileName);
 
             List<JsonConverter> converters = new List<JsonConverter>();
@@ -197,6 +228,14 @@ namespace PartsInjector
             var gliderParts = Runtime.GetGliderParts();
             var driverParts = Runtime.GetDriverParts();
 
+            var settings = new JsonSerializerSettings()
+            {
+                Converters = converters.ToArray(),
+                FloatParseHandling = FloatParseHandling.Decimal,
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                Formatting = Formatting.None,
+            };
+
             for (int i = 0; i < bodyParts.Count; i++)
             {
                 string name = bodyParts[i].PartName;
@@ -207,7 +246,7 @@ namespace PartsInjector
 
                     Console.WriteLine($"Injecting Body {name}");
 
-                    bodyParts[i] = JsonConvert.DeserializeObject<BodyObject>(File.ReadAllText(partFileName), converters.ToArray());
+                    bodyParts[i] = JsonConvert.DeserializeObject<BodyObject>(File.ReadAllText(partFileName), settings);
                     bodyParts[i].UpdateSetters();
 
                     //Body has 3 sets of additional param tables
@@ -220,7 +259,7 @@ namespace PartsInjector
                         partFileName = $"Body/{name}/TireParams/{bodyTR[j].Name}.json";
                         if (File.Exists(partFileName))
                         {
-                            bodyTR[j] = JsonConvert.DeserializeObject<PartsLibrary.BodyTireObject>(File.ReadAllText(partFileName), converters.ToArray());
+                            bodyTR[j] = JsonConvert.DeserializeObject<PartsLibrary.BodyTireObject>(File.ReadAllText(partFileName), settings);
                             bodyTR[j].TireIndex = j; //Update index as json is edited
                             Runtime.BodyIndex = i;
                             bodyTR[j].UpdateSetters();
@@ -232,7 +271,7 @@ namespace PartsInjector
                          partFileName = $"Body/{name}/GliderParams/{bodyGL[j].Name}.json";
                          if (File.Exists(partFileName))
                          {
-                             bodyGL[j] = JsonConvert.DeserializeObject<PartsLibrary.BodyGliderObject>(File.ReadAllText(partFileName), converters.ToArray());
+                             bodyGL[j] = JsonConvert.DeserializeObject<PartsLibrary.BodyGliderObject>(File.ReadAllText(partFileName), settings);
                              bodyGL[j].GliderIndex = j; //Update index as json is edited
                             Runtime.BodyIndex = i;
                             bodyGL[j].UpdateSetters();
@@ -244,7 +283,7 @@ namespace PartsInjector
                          partFileName = $"Body/{name}/DriverParams/{bodyDR[j].Name}.json";
                          if (File.Exists(partFileName))
                          {
-                             bodyDR[j] = JsonConvert.DeserializeObject<PartsLibrary.BodyDriverObject>(File.ReadAllText(partFileName), converters.ToArray());
+                             bodyDR[j] = JsonConvert.DeserializeObject<PartsLibrary.BodyDriverObject>(File.ReadAllText(partFileName), settings);
                              bodyDR[j].DriverIndex = j; //Update index as json is edited
                             Runtime.BodyIndex = i;
                             bodyDR[j].UpdateSetters();
@@ -262,7 +301,7 @@ namespace PartsInjector
                 {
                     Runtime.TireIndex = i;
 
-                    tireParts[i] = JsonConvert.DeserializeObject<TireObject>(File.ReadAllText(partFileName), converters.ToArray());
+                    tireParts[i] = JsonConvert.DeserializeObject<TireObject>(File.ReadAllText(partFileName), settings);
                     tireParts[i].UpdateSetters();
                     Console.WriteLine($"Injecting Tire {name}");
                 }
@@ -277,7 +316,7 @@ namespace PartsInjector
                 {
                     Runtime.GliderIndex = i;
 
-                    gliderParts[i] = JsonConvert.DeserializeObject<GliderObject>(File.ReadAllText(partFileName), converters.ToArray());
+                    gliderParts[i] = JsonConvert.DeserializeObject<GliderObject>(File.ReadAllText(partFileName), settings);
                     gliderParts[i].UpdateSetters();
                     Console.WriteLine($"Injecting Glider {name}");
                 }
@@ -292,7 +331,7 @@ namespace PartsInjector
                 {
                     Runtime.GliderIndex = i;
 
-                    driverParts[i] = JsonConvert.DeserializeObject<DriverObject>(File.ReadAllText(partFileName), converters.ToArray());
+                    driverParts[i] = JsonConvert.DeserializeObject<DriverObject>(File.ReadAllText(partFileName), settings);
                     driverParts[i].UpdateSetters();
                     Console.WriteLine($"Injecting Driver {name}");
                 }
